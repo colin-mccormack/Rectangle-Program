7dad95e9a4ae58439753f551d6125c7db327798a
MrMouse2405 <68935395+MrMouse2405@users.noreply.github.com>
2021-12-12 13:59:58 -0500

    -

--- Lib/LinkedHashMap/LinkedHashMap.h
+++ Lib/LinkedHashMap/LinkedHashMap.h
@@ -85,23 +85,142 @@ typedef struct {
      */
 
 
-
     int (*const canStore)(LinkedHashMapType);
+
+    /*
+
+    void put(LinkedHashMapType,const char * Key, void * Value)
+
+    Appends an item into the hash map
+
+    params:
+        LinkedHashMapType, StringKey, Value
+
+    Usage:
+        LinkedHashMap->put()
+
+    */
+
 
     void (*const put)(LinkedHashMapType, char *__restrict StringKey, void *__restrict Instance);
 
+
+    /*
+
+    void putAt(LinkedHashMapType,const char * Key, void * Value, int Index)
+
+    Inserts a value into the hashmap at a given index.
+
+    params:
+        LinkedHashMapType, StringKey, Value, Index
+
+    Usage:
+        LinkedHashMap->putAt()
+
+    */
+
     void (*const putAt)(LinkedHashMapType, char *__restrict StringKey, void *__restrict Instance, int Index);
+
+
+    /*
+
+    void* get(LinkedHashMapType,const char * Key)
+
+    gets a value that is indexed by the given string
+
+    params:
+        LinkedHashMapType, StringKey
+
+    Usage:
+        LinkedHashMap->getByKey()
+
+    */
 
     void *(*const getByKey)(LinkedHashMapType, const char *__restrict StringKey);
 
+    /*
+
+    void* getByIndex(LinkedHashMapType,int Index)
+
+    Returns a value present at a given integer index
+
+    params:
+        LinkedHashMapType, Index
+
+    Usage:
+        LinkedHashMap->getByIndex()
+
+    */
+
     void *(*const getByIndex)(LinkedHashMapType, const int Index);
+
+
+    /*
+
+    void forEach(LinkedHashMapType,
+                          void(*f)(const int Index, const char *__restrict StringKey, const void *__restrict Value))
+
+    Calls function f on each value present
+
+    params:
+        LinkedHashMapType, void(*f)(Index,StringKey,Value)
+
+    Usage:
+        LinkedHashMap->forEach()
+
+    */
 
     void (*const forEach)(LinkedHashMapType,
                           void(*f)(const int Index, const char *__restrict StringKey, const void *__restrict Value));
 
+    /*
+
+    void DeleteKey(LinkedHashMapType,StringKey)
+
+    Deletes the value indexed by the String Key
+
+    params:
+        LinkedHashMapType, StringKey
+
+    Usage:
+        LinkedHashMap->DeleteKey()
+
+    */
+
+
     void (*const DeleteKey)(LinkedHashMapType, const char *__restrict StringKey);
 
+    /*
+
+    void DeleteIndex(LinkedHashMapType,int Index)
+
+    Deletes the value present at a given integer index
+
+    params:
+        LinkedHashMapType, Index
+
+    Usage:
+        LinkedHashMap->DeleteIndex()
+
+    */
+
+
     void (*const DeleteIndex)(LinkedHashMapType, const int Index);
+
+    /*
+
+    void DeleteMap(LinkedHashMapType)
+
+    Deletes the entire map.
+
+    params:
+        LinkedHashMapType, Index
+
+    Usage:
+        LinkedHashMap->put()
+
+    */
+
 
     void (*const DeleteMap)(LinkedHashMapType);
 
